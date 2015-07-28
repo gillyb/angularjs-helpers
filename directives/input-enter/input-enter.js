32459384bfffd581ffc1990c1d81c0b3cb578b4a
Gilly Barr <gillyb@gmail.com>
2015-07-28 18:02:04 +0300

    adding input-enter directive

---
+++ directives/input-enter/input-enter.js
@@ -0,0 +1,16 @@
+angular.module('myModule').directive('enterKey', [function() {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if (event.which === 13) {
+                    scope.$apply(function () {
+                        scope.$eval(attrs.enterKey, {'event': event});
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        }
+    };
+}]);
